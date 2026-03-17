2e0871a943222792ad69b23122e90c0125af26e4
Vidya jha <jha082729@gmail.com>
2026-03-18 01:17:33 +0530

    Implement stock and earnings data fetching functions
    Added functions to fetch stock and earnings data from an API.

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,1 +1,31 @@
+import requests
+import pandas as pd
 
+API_KEY = "acHoNoczANkNzrIXD2tIQmWWjfFguKjy"
+
+def get_stock_data(symbol="AAPL"):
+    
+    url = f"https://financialmodelingprep.com/stable/historical-price-eod/full?symbol={symbol}&apikey={API_KEY}"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    df = pd.DataFrame(data)
+    
+    df["date"] = pd.to_datetime(df["date"])
+    
+    return df
+
+
+def get_earnings_data(symbol="AAPL"):
+    
+    url = f"https://financialmodelingprep.com/stable/earnings-calendar?symbol={symbol}&apikey={API_KEY}"
+    
+    response = requests.get(url)
+    data = response.json()
+    
+    df = pd.DataFrame(data)
+    
+    df["date"] = pd.to_datetime(df["date"])
+    
+    return df
